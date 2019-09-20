5a0ebddf27009a8c83bb8daf23d5c1d4ef4c0186
Mohammad Rezaei <mrezaei67@yahoo.com>
2019-09-21 01:56:31 +0430

    clean code

--- authentication/serializsers.py
+++ authentication/serializsers.py
@@ -33,39 +33,6 @@ class SignupSerializer(serializers.ModelSerializer):
             raise ValidationError('Tha password and re_password are not same.')
         return attrs
 
-# class VerifySerializer(serializers.ModelSerializer, ErrorHandlerSerializerMixin):
-#     class Meta:
-#         model = User
-#         fields = ['email',
-#                   'verify_code']
-#
-#     verify_code = serializers.CharField(max_length=40)
-#     email = serializers.EmailField(max_length=255)
-#     def _expire(self, verify_request):
-#         verify_request.soft_deleted = True
-#         verify_request.save()
-#
-#     def validate(self, attrs):
-#         verify_code = attrs.get('verify_code')
-#         email = attrs.get('email')
-#         # self.verify_type = attrs.get('verify_type')
-#         verify_request=VerifyRequest.objects.filter(email=email,soft_deleted=False).first()
-#
-#         if verify_request  or verify_code==GOD_SMS_CODE :
-#             if verify_request.verification_code == verify_code or verify_code==GOD_SMS_CODE:
-#                 # if (verify_request.created_date + timedelta(seconds=COUNTER_SECONDS)) < timezone.now():
-#                 #     self._expire(verify_request)
-#                 #     raise ValidationError("Verification code is invalid")
-#                 self._expire(verify_request)
-#             else:
-#                 verify_request.counter_tries += 1
-#                 verify_request.soft_deleted = verify_request.counter_tries >= COUNTER_TRIES
-#                 verify_request.save()
-#                 raise ValidationError('کد تایید معتبر نیست.')
-#         else:
-#             raise ValidationError('کد تایید معتبر نیست.')
-#         return attrs
-
 
 class LoginPassSerializer(serializers.Serializer):
     email = serializers.EmailField()
