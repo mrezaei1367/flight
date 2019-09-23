4a5d9bf11b1dd88595685b041cb9631c0d887977
rezaei <app.postman@gmail.com>
2019-09-23 18:31:09 +0330

    add session login for swagger

--- flight/settings/default_settings.py
+++ flight/settings/default_settings.py
@@ -91,6 +91,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
